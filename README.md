234de85fdc01e858f99ec21ce00c0e8cd92ed58b
Saad Sohail <rishat.5081@gmail.com>
2020-12-21 12:50:47 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Multer File Upload
+
+This is a NPM package which is used same like express fileupload
+
+## How this will work.
+
+1. Git clone or download it as zip 
+
+2. Extract files 
+
+3. Do `npm i`
+
+4. Then open terminal and write `npm start`
+
+5. Go to localhost `http://localhost:1111/`
---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express'),
+    app = express(),
+    multer = require('multer'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    fs = require('file-system'),
+    fss = require('fs'),
+    bcrypt = require('bcrypt')
+
+var userName = 'saaaad'
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.use(express.static('./public'))
+//fss.renameSync('public/pic/saad.png','public/pic/userpic-1572516779307.png')
+
+
+
+// const mstorage = multer.diskStorage(
+//     {
+//         destination: './public/pic/',
+//         filename: (req, file, cb) => {
+//             console.log(req.body)
+//             cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+//         }
+//     })
+
+const upload_it = multer({
+    storage: multer.diskStorage({
+        destination: './public/pic/',
+        filename: (req, file, cb) => {
+            cb(null, file.fieldname + '-' + path.extname(file.originalname))
+        }
+    }),
+    limits: {
+        fileSize: 3000000
+    },
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb)
+    }
+}).any()
+
+
+function checkFileType(file, cb) {
+
+    const types = /jpeg|jpg|png|gif/
+    const extnames = types.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = types.test(file.mimetype)
+    console.log(extnames, mimetype);
+
+    if (extnames && mimetype)
+        return cb(null, true)
+    else
+        cb('Wrong File Type')
+}
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+app.post('/picUpload', (req, res) => {
+    upload_it(req, res, (error) => {
+        if (error)
+            res.render('index', {
+                message: error
+            })
+        else {
+            console.log(req.files)
+            fss.renameSync(req.files[0].path, req.files[0].destination + req.body.user + path.extname(req.files[0].originalname))
+            console.log(req.body.user + path.extname(req.files[0].originalname))
+            res.status(200).render('uploaded', {
+                file: req.body.user + path.extname(req.files[0].originalname)
+            })
+        }
+    })
+})
+
+app.listen(1111, (req, res) => {
+    console.log('Server Started at port 1111')
+})
